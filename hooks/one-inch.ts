1745972703777073e0b8b15d4ebc572b60695122
Sevak <sargsyansevak9312@gmail.com>
2024-04-10 09:48:54 +0400

    remove logs

--- hooks/one-inch.ts
+++ hooks/one-inch.ts
@@ -6,30 +6,21 @@ import {ROUTER_ADDRESSES_1INCH} from "@/utils/constants";
 import {generate1InchSwapParmas, getSigner} from "@/utils/helpers";
 import isZero from "@/utils/isZero";
 import {BigNumber} from "@ethersproject/bignumber";
-import {useWeb3React} from "@web3-react/core";
-import {useWeb3ModalProvider, useWeb3ModalAccount} from '@web3modal/ethers/react'
+import {useWeb3ModalProvider} from '@web3modal/ethers/react'
 import {BrowserProvider} from "ethers";
 
 
 export const useSwap1Inch = ({from, to, chainId, typedValue, account}) => {
-    // const chainId = 1;
-    // const { account, library } = useWeb3React();
     const {walletProvider} = useWeb3ModalProvider();
-    console.log('walletProvider', walletProvider);
+
     if (!walletProvider) return;
-    // const { address: account, isConnected } = useWeb3ModalAccount();
+
     const library = new BrowserProvider(walletProvider);
-    // const typedValue = 1; // TO DO: get from input
     const router1Inch = ROUTER_ADDRESSES_1INCH[chainId];
-    // console.log('aaa,llll',account,library);
+
     if (!account) return;
     
-    // const from = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"; // TO DO: set address from
-    
-    // const to = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"; // TO DO: set address to
-    
     const swap1Inch = async () => {
-        console.log('swapinch', from, to, account, typedValue, library);
         
         const swapParams = generate1InchSwapParmas(
             from,
@@ -44,8 +35,8 @@ export const useSwap1Inch = ({from, to, chainId, typedValue, account}) => {
             walletAddress: account
         });
         
-        if (!allowance || allowance === '0') return; // @TODO
-        
+        if (!allowance || allowance === '0') return;
+        // If allowance is less than requested amount then don't process(@TODO include also gas calculation)
         if (BigNumber.from(allowance).lt(swapParams.amount)) return;
         
         await new Promise((resolve) => setTimeout(resolve, 1000));
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/**/*.{js,ts,jsx,tsx,mdx}",
-    // "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    // "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
-    // "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
 
