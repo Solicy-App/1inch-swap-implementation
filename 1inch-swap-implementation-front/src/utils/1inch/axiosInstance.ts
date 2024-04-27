7de1a7acc88d83b96d717fed673b934aff19477d
Tigran <114810903+Tigran5811@users.noreply.github.com>
2024-04-27 16:09:28 +0400

    Update axiosInstance.ts

--- 1inch-swap-implementation-front/src/utils/1inch/axiosInstance.ts
+++ 1inch-swap-implementation-front/src/utils/1inch/axiosInstance.ts
@@ -1,27 +1,27 @@
-import "dotenv/config";
-import axios, { AxiosError } from "axios";
+// import "dotenv/config";
+// import axios, { AxiosError } from "axios";
 
-const axios1Inch = axios.create({
-  baseURL: "https://api.1inch.dev" || "",
-  headers: {
-    Authorization: `Bearer IPD4zPSrn8M3kWWGq6mRfO5OSw3sCoSF`,
-    accept: "application/json",
-    "Access-Control-Allow-Origin": "*",
-  },
-});
+// const axios1Inch = axios.create({
+//   baseURL: "https://api.1inch.dev" || "",
+//   headers: {
+//    Authorization: `Bearer ${process.env.REACT_APP_API_KEY_1INCH}`,
+//     accept: "application/json",
+//     "Access-Control-Allow-Origin": "*",
+//   },
+// });
 
-axios1Inch.interceptors.response.use(
-  (response) => response,
-  async (error: AxiosError) => {
-    if (error.response && error.response.status === 429) {
-      // If status code is 429, wait for 1 second and retry the request
-      if (!error.config) return;
+// axios1Inch.interceptors.response.use(
+//   (response) => response,
+//   async (error: AxiosError) => {
+//     if (error.response && error.response.status === 429) {
+//       // If status code is 429, wait for 1 second and retry the request
+//       if (!error.config) return;
 
-      await new Promise((resolve) => setTimeout(resolve, 1000)); // 1000 ms = 1 second
-      return axios1Inch.request(error.config); // Retry the request
-    }
-    return Promise.reject(error);
-  }
-);
+//       await new Promise((resolve) => setTimeout(resolve, 1000)); // 1000 ms = 1 second
+//       return axios1Inch.request(error.config); // Retry the request
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
-export default axios1Inch;
+// export default axios1Inch;
